3f519fbb6b02a8d284e465b5884ebfaf5d5e564e
mr_shinkoudan <160583802+mplankton@users.noreply.github.com>
2024-07-23 10:30:37 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -33,8 +33,8 @@ public class Main extends JFrame implements KeyListener {
         }
     }
     Main(JPanel p) {
-        ImageIcon icon = new ImageIcon("C:\\Mods\\threeJava\\src\\main\\java\\org\\example\\icon.png");
-        setTitle("Hey guys! We have a gift for you!");
+        ImageIcon icon = new ImageIcon("your icon's file space!");
+        setTitle("your frame's name");
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(width,height);
